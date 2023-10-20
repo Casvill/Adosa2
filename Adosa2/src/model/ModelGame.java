57a8624b4fcbc8f2aee732b7602e1a294ec26048
Casvill <casvilldaniel@gmail.com>
2023-10-19 23:27:47 -0500

    Some tests

--- Adosa2/src/model/ModelGame.java
+++ Adosa2/src/model/ModelGame.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 /**
  *
  * @author Daniel Casvill
  */
-public class ModelGame {
+public class ModelGame 
+{
+    private short hits;
+    private short failures;
+    private int punctuation;
+    private byte lives;
+
+    public ModelGame() 
+    {
     
+    }
+
+    public ModelGame(short hits, short failures, int punctuation, byte lives) 
+    {
+        this.hits = hits;
+        this.failures = failures;
+        this.punctuation = punctuation;
+        this.lives = lives;
+    }
+
+    public short getHits() 
+    {
+        return hits;
+    }
+
+    public void setHits(short hits) 
+    {
+        this.hits = hits;
+    }
+
+    public short getFailures() 
+    {
+        return failures;
+    }
+
+    public void setFailures(short failures) 
+    {
+        this.failures = failures;
+    }
+
+    public int getPunctuation() 
+    {
+        return punctuation;
+    }
+
+    public void setPunctuation(int punctuation) 
+    {
+        this.punctuation = punctuation;
+    }
+
+    public byte getLives() 
+    {
+        return lives;
+    }
+
+    public void setLives(byte lives) 
+    {
+        this.lives = lives;
+    }                      
 }
